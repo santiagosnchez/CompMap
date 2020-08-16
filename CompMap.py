fca92fb2de7ecab0c0cc1e6afe11ce578285c01d
Santiago Sanchez-Ramirez <santiago.snchez@gmail.com>
2020-08-16 03:28:45 -0400

    Revamped, working version.

--- CompMap.py
+++ CompMap.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python env
 import sys
 import argparse
+#import itertools
+import art
 try:
     import pysam
 except ImportError as er:
@@ -11,6 +13,8 @@ pip install pysam\n""")
 
 def main():
     # parse arguments
+    pretty_title = art.text2art("CompMap")
+    print(pretty_title)
     parser = argparse.ArgumentParser(prog="CompMap.py",
     formatter_class=argparse.RawTextHelpFormatter,
     description="""
@@ -22,11 +26,11 @@ def main():
 Examples:
 python CompMap.py -1 first.bam -2 second.bam -r read_name_list -b my_base_name -s1 sp1 -s2 sp2
 
-Example for read list:
-ABC-HG000:000:XXXXXXX:1:0010:001:100
-ABC-HG000:000:XXXXXXX:1:0010:001:110
-ABC-HG000:000:XXXXXXX:1:0010:001:130
-ABC-HG000:000:XXXXXXX:1:0010:001:110
+Example of BED file:
+chr1\t0\t1000\tgene1
+chr1\t2500\t3000\tgene2
+chr2\t0\t2500\tgene3
+chr3\t150\t1000\tgene4
     \n""")
     parser.add_argument(
     '--bam1', '-1', type=str, required=True,
@@ -35,8 +39,11 @@ ABC-HG000:000:XXXXXXX:1:0010:001:110
     '--bam2', '-2', type=str, required=True,
     help='second bam file.')
     parser.add_argument(
-    '--reads', '-r', type=str, required=True,
-    help='a list of reads names')
+    '--bed1', '-b1', type=str, required=True,
+    help='first bam file.')
+    parser.add_argument(
+    '--bed2', '-b2', type=str, required=True,
+    help='second bam file.')
     parser.add_argument(
     '--base', '-b', type=str, default="out",
     help='base name for your output files (recommended).')
@@ -54,11 +61,8 @@ ABC-HG000:000:XXXXXXX:1:0010:001:110
     help='string suffix used to distinguish matches, mismatches, and ambiguous reads in -2. Default: 2')
     args = parser.parse_args()
 
-    # read read names
-    reads = read_readNames(args.reads)
-    print("done reading read names:",args.reads)
-
     # read bam files
+    print("reading BAM files")
     bam1 = pysam.AlignmentFile(args.bam1, 'rb')
     bam2 = pysam.AlignmentFile(args.bam2, 'rb')
 
@@ -68,15 +72,62 @@ ABC-HG000:000:XXXXXXX:1:0010:001:110
     print("indexing file by read name:",args.bam2)
     name_indexed2 = index_by_readName(bam2)
 
-    # compare reads
-    comp_map(bam1, bam2, name_indexed1, name_indexed2, reads, args.base, args.suffix1, args.suffix2, args.AS_tag, args.NM_tag)
-    print("done")
+    # copy header
+    header1 = bam1.header.copy()
+    header2 = bam2.header.copy()
+    # output files
+    out = (args.base,args.suffix1,args.suffix2)
 
-# to read read names
-def read_readNames(file):
-    with open(file, "r") as f:
-        reads = f.read().splitlines()
-    return reads
+    # read bed files
+    print("reading BED files")
+    bed1 = read_bed(args.bed1)
+    bed2 = read_bed(args.bed2)
+
+    # compare reads
+    comp_map(bed1, bed2, bam1, bam2, name_indexed1, name_indexed2, out, args.AS_tag, args.NM_tag)
+
+# function to compare reads
+def comp_map(bed1, bed2, bam1, bam2, name_indexed1, name_indexed2, out, as_tag, nm_tag):
+    # process bed files
+    # check if gene ids are the same
+    print("checking gene_id names: ", end="")
+    bed1_gn = [ x[3] for x in bed1 ]
+    bed2_gn = [ x[3] for x in bed2 ]
+    bed1_gn.sort()
+    bed2_gn.sort()
+    if bed1_gn != bed2_gn:
+        raise NameError('Gene ids in BED files are not the same.')
+        sys.exit(1)
+    print("pass")
+
+    # change to dictionaries
+    bed1 = { x[3]:x for x in bed1 }
+    bed2 = { x[3]:x for x in bed2 }
+
+    # start counter
+    feature_counter = 0
+
+    # open outfile and write header
+    print("writting to: "+out[0]+"_counts.txt")
+    o = open(out[0]+"_counts.txt", "w")
+    o.write("gene_id\t"+out[0]+"_"+out[1]+"\t"+out[0]+"_"+out[2]+"\n")
+
+    # loop through bedfiles
+    for geneid in bed1_gn:
+        feature_counter += 1
+        bedline1 = bed1[geneid]
+        bedline2 = bed2[geneid]
+        # count reads
+        counts1 = cm_count_reads(bedline1, bam1, name_indexed1, bam2, name_indexed2, as_tag, nm_tag)
+        counts2 = cm_count_reads(bedline2, bam2, name_indexed2, bam1, name_indexed1, as_tag, nm_tag)
+        # correct counts
+        corr1, corr2 = correct_counts(counts1, counts2)
+        o.write(geneid+"\t"+str(corr1)+"\t"+str(corr2)+"\n")
+
+        if feature_counter % 100 == 0:
+            print("processed "+str(feature_counter)+" features")
+    o.close()
+    print("done")
 
 # index bam by read name
 def index_by_readName(bam):
@@ -84,100 +135,76 @@ def index_by_readName(bam):
     name_indexed.build()
     return name_indexed
 
-# function to compare reads
-def comp_map(bam1, bam2, name_indexed1, name_indexed2, reads, outbase, suffix1, suffix2, as_tag, nm_tag):
-    # count alignments
-    def count_aln(aln):
-        c = 0
-        for a in aln:
-            c += 1
-        return c
+def read_bed(file):
+    with open(file, "r") as f:
+        bed = [ x.split("\t") for x in f.read().splitlines() ]
+    return bed
 
+def cm_count_reads(bedline, bam, bam_ni, bam_alt, bam_ni_alt, as_tag, nm_tag):
+    # check unique alignment
+    def check_unique(read_name, bam_ni):
+        c = 0
+        for r in bam_ni.find(read_name):
+            c += 1
+        if c > 1:
+            return False
+        else:
+            return True
+    # check if read is present
+    def check_is_found(read_name, bam_ni):
+        try:
+            bam_ni.find(read_name)
+        except KeyError:
+            return False
+        else:
+            return True
     # return score and mismatches
     def get_as_nm(x, as_tag, nm_tag):
         return x.get_tag(as_tag), x.get_tag(nm_tag)
-
-    def write_aln(x, out, tag):
-        x.tags += [('CM',tag)]
-        out.write(x)
-
-    def compare_and_write(x1, x2, as_tag, nm_tag, out1, out2):
-        score1, mismatches1 = get_as_nm(x1, as_tag, nm_tag)
-        score2, mismatches2 = get_as_nm(x2, as_tag, nm_tag)
-        if score1 > score2:
-            write_aln(x1, out1, 'M')
-        elif score1 < score2:
-            write_aln(x2, out2, 'M')
+    # compare reads based on AS and NM
+    def compare_reads(x, x_alt, as_tag, nm_tag):
+        score, mismatches = get_as_nm(x, as_tag, nm_tag)
+        score_alt, mismatches_alt = get_as_nm(x_alt, as_tag, nm_tag)
+        if score > score_alt:
+            return True
+        elif mismatches < mismatches_alt:
+            return True
         else:
-            if mismatches1 < mismatches2:
-                write_aln(x1, out1, 'M')
-            elif mismatches1 > mismatches2:
-                write_aln(x2, out2, 'M')
+            return False
+    #
+    count_all = 0; count_bmt = 0
+    for r in bam.fetch(bedline[0], int(bedline[1]), int(bedline[2])):
+        if not any([r.is_unmapped, r.is_secondary, r.is_supplementary]) and check_unique(r.qname, bam_ni):
+            count_all += 1
+            if check_is_found(r.qname, bam_ni_alt) and check_unique(r.qname, bam_ni_alt):
+                r_alt = next(bam_ni_alt.find(r.qname))
+                if not any([r_alt.is_unmapped, r_alt.is_secondary, r_alt.is_supplementary]):
+                    if compare_reads(r, r_alt, as_tag, nm_tag):
+                        count_bmt += 1
+                else:
+                    count_bmt += 1
             else:
-                write_aln(x1, out1, 'A')
-                write_aln(x2, out2, 'A')
+                count_bmt += 1
+    return count_all, count_bmt
 
-    # copy header
-    header1 = bam1.header.copy()
-    header2 = bam2.header.copy()
-    # output files
-    out1 = pysam.Samfile(outbase+"_"+suffix1+".bam", 'wb', header=header1)
-    out2 = pysam.Samfile(outbase+"_"+suffix2+".bam", 'wb', header=header2)
-    #out_amb1 = pysam.Samfile(outbase+"_amb_"+suffix1+".bam", 'wb', header=header1)
-    #out_amb2 = pysam.Samfile(outbase+"_amb_"+suffix2+".bam", 'wb', header=header2)
-    counter = 0
-    for name in reads:
-        counter += 1
-        if name[0] == "@":
-            name = name[1:]
-        name = name.split(" ")[0]
-        try:
-            x = name_indexed1.find(name)
-        except KeyError:
-            try:
-                x = name_indexed2.find(name)
-            except KeyError:
-                pass
-            else:
-                if count_aln(x) == 1:
-                    x = next(x)
-                    if not any([x.is_unmapped, x.is_secondary, x.is_supplementary]):
-                        write_aln(x, out2, 'M')
+def correct_counts(counts1, counts2):
+    if counts1[0] == counts1[1] and counts2[0] == counts2[1]:
+        return counts1[0], counts2[0]
+    else:
+        if counts1[1] < counts2[1]:
+            prop = counts1[1]/counts2[1]
+            corrected1 = counts1[1] + ((counts1[0]-counts1[1]) * prop)
+            corrected2 = counts2[1] + ((counts2[0]-counts2[1]) * 1-prop)
+            return int(corrected1), int(corrected2)
+        elif counts1[1] > counts2[1]:
+            prop = counts2[1]/counts1[1]
+            corrected1 = counts1[1] + ((counts1[0]-counts1[1]) * 1-prop)
+            corrected2 = counts2[1] + ((counts2[0]-counts2[1]) * prop)
+            return int(corrected1), int(corrected2)
         else:
-            try:
-                x = name_indexed2.find(name)
-            except KeyError:
-                if count_aln(x) == 1:
-                    x = next(x)
-                    if not any([x.is_unmapped, x.is_secondary, x.is_supplementary]):
-                        write_aln(x, out1, 'M')
-            else:
-                # count alignments for each read
-                unique_matches1 = count_aln(name_indexed1.find(name))
-                unique_matches2 = count_aln(name_indexed2.find(name))
-                # check if alignments are unique
-                if unique_matches1 == 1 and unique_matches2 == 1:
-                    x1 = next(name_indexed1.find(name))
-                    x2 = next(name_indexed2.find(name))
-                    # test if reads are mapped
-                    if not any([x1.is_unmapped, x1.is_secondary, x1.is_supplementary]) and not any([x2.is_unmapped, x2.is_secondary, x2.is_supplementary]):
-                        compare_and_write(x1, x2, as_tag, nm_tag, out1, out2)
-                    elif not any([x1.is_unmapped, x1.is_secondary, x1.is_supplementary]) and any([x2.is_unmapped, x2.is_secondary, x2.is_supplementary]):
-                        write_aln(x1, out1, 'M')
-                    elif any([x1.is_unmapped, x1.is_secondary, x1.is_supplementary]) and not any([x2.is_unmapped, x2.is_secondary, x2.is_supplementary]):
-                        write_aln(x2, out2, 'M')
-                elif unique_matches1 == 1 and unique_matches2 > 1:
-                    x1 = next(name_indexed1.find(name))
-                    if not any([x1.is_unmapped, x1.is_secondary, x1.is_supplementary]):
-                        write_aln(x1, out1, 'M')
-                elif unique_matches1 > 1 and unique_matches2 == 1:
-                    x2 = next(name_indexed2.find(name))
-                    if not any([x2.is_unmapped, x2.is_secondary, x2.is_supplementary]):
-                        write_aln(x2, out2, 'M')
-        if counter % 100000 == 0:
-            print("parsed "+str(counter)+" records")
-    out1.close()
-    out2.close()
+            corrected1 = counts1[1] + ((counts1[0]-counts1[1]) * .5)
+            corrected2 = counts2[1] + ((counts2[0]-counts2[1]) * .5)
+            return int(corrected1), int(corrected2)
 
 if __name__ == "__main__":
     main()
