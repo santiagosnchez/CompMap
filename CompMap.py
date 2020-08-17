df6459864c946a6fbbc5e656c45d86432da7a4ff
Santiago Sanchez-Ramirez <santiago.snchez@gmail.com>
2020-08-17 11:59:09 -0400

    Added cool progress bar.

--- CompMap.py
+++ CompMap.py
@@ -3,6 +3,7 @@ import sys
 import argparse
 #import itertools
 import art
+import time
 try:
     import pysam
 except ImportError as er:
@@ -62,14 +63,14 @@ chr3\t150\t1000\tgene4
     args = parser.parse_args()
 
     # read bam files
-    print("reading BAM files")
+    print("Reading BAM files.")
     bam1 = pysam.AlignmentFile(args.bam1, 'rb')
     bam2 = pysam.AlignmentFile(args.bam2, 'rb')
 
     # index by read name
-    print("indexing file by read name:",args.bam1)
+    print("Indexing file by read name:",args.bam1)
     name_indexed1 = index_by_readName(bam1)
-    print("indexing file by read name:",args.bam2)
+    print("Indexing file by read name:",args.bam2)
     name_indexed2 = index_by_readName(bam2)
 
     # copy header
@@ -79,7 +80,7 @@ chr3\t150\t1000\tgene4
     out = (args.base,args.suffix1,args.suffix2)
 
     # read bed files
-    print("reading BED files")
+    print("Reading BED files")
     bed1 = read_bed(args.bed1)
     bed2 = read_bed(args.bed2)
 
@@ -90,7 +91,7 @@ chr3\t150\t1000\tgene4
 def comp_map(bed1, bed2, bam1, bam2, name_indexed1, name_indexed2, out, as_tag, nm_tag):
     # process bed files
     # check if gene ids are the same
-    print("checking gene_id names: ", end="")
+    print("Checking gene_id names: ", end="")
     bed1_gn = [ x[3] for x in bed1 ]
     bed2_gn = [ x[3] for x in bed2 ]
     bed1_gn.sort()
@@ -98,7 +99,8 @@ def comp_map(bed1, bed2, bam1, bam2, name_indexed1, name_indexed2, out, as_tag, 
     if bed1_gn != bed2_gn:
         raise NameError('Gene ids in BED files are not the same.')
         sys.exit(1)
-    print("pass")
+    print("pass.")
+    total_features = len(bed1_gn)
 
     # change to dictionaries
     bed1 = { x[3]:x for x in bed1 }
@@ -108,11 +110,12 @@ def comp_map(bed1, bed2, bam1, bam2, name_indexed1, name_indexed2, out, as_tag, 
     feature_counter = 0
 
     # open outfile and write header
-    print("writting to: "+out[0]+"_counts.txt")
+    print("Writting to: "+out[0]+"_counts.txt")
     o = open(out[0]+"_counts.txt", "w")
     o.write("gene_id\t"+out[0]+"_"+out[1]+"\t"+out[0]+"_"+out[2]+"\n")
 
     # loop through bedfiles
+    t1 = time.time()
     for geneid in bed1_gn:
         feature_counter += 1
         bedline1 = bed1[geneid]
@@ -124,10 +127,13 @@ def comp_map(bed1, bed2, bam1, bam2, name_indexed1, name_indexed2, out, as_tag, 
         corr1, corr2 = correct_counts(counts1, counts2)
         o.write(geneid+"\t"+str(corr1)+"\t"+str(corr2)+"\n")
 
-        if feature_counter % 100 == 0:
-            print("processed "+str(feature_counter)+" features")
+        # print progress bar
+        progress = make_progress_bar(feature_counter, total_features, t1, 70)
+        print("\r", progress[0] % progress[1:], end='', flush=True)
+        # if feature_counter % 100 == 0:
+        #     print("processed "+str(feature_counter)+" features")
     o.close()
-    print("done")
+    print("\nDone processing "+str(feature_counter)+" features.")
 
 def cm_count_reads(bedline, bam, bam_ni, bam_alt, bam_ni_alt, as_tag, nm_tag):
     # check unique alignment
@@ -215,6 +221,18 @@ def read_bed(file):
     with open(file, "r") as f:
         bed = [ x.split("\t") for x in f.read().splitlines() ]
     return bed
+
+def make_progress_bar(rec, total, t1, width):
+    i = (rec/total * 100) % 100
+    if i != 0:
+        asterix = "*" * int(i * (width/100))
+        dashes = "-" * (width-int(i * width/100))
+    else:
+        asterix = "*" * width
+        dashes = ""
+    t2 = time.time()
+    elapsed = t2-t1
+    return "|"+asterix+dashes+"| "+"%5.2f%% %7.2f s", i, elapsed
 
 if __name__ == "__main__":
     main()
